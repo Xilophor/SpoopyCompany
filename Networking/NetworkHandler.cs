eee3c9b1c7ff8751183c3737b45a82abef43fdf5
Xilophor <ryanzgreg@gmail.com>
2023-12-01 09:27:28 -0500

    Removed junk comments

--- Networking/NetworkHandler.cs
+++ Networking/NetworkHandler.cs
@@ -7,21 +7,9 @@ namespace SpoopyComponents
 {
     public class NetworkHandler : NetworkBehaviour
     {
-        /*private void Awake()
-        {
-            if (NetworkHandler.Instance == null)
-            {
-                NetworkHandler.Instance = this;
-                return;
-            }
-            Object.Destroy(NetworkHandler.Instance.gameObject);
-            NetworkHandler.Instance = this;
-        }*/
 
         public override void OnNetworkSpawn()
         {
-            // if (!IsServer || !IsHost && IsOwner) {
-            // }
             Debug.Log("SpoopyCompany - NetworkHandler created");
             OnEvent = null;
         }
@@ -35,6 +23,5 @@ namespace SpoopyComponents
         }
 
         public static event Action<string> OnEvent;
-	    //public static NetworkHandler Instance { get; private set; }
     }
 }
